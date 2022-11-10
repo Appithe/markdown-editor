865de3f4344fe7daddb50325cc6092052ecc552a
appithe <joelaviles413@gmail.com>
2022-11-09 22:49:18 -0600

    finish markdow v1.0.0

---
+++ src/App.svelte
@@ -0,0 +1,64 @@
+<!-- TODO: componetizar -->
+<!-- TODO: cambiar remarkable a @haakonmb/markdown-draft-js y estilar output -->
+<style>
+	main {
+		background: #0c1618;
+		margin: 0;
+		padding: 2rem;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: row;
+		gap: 2rem;
+	}
+
+	textarea {
+		background: #004643;
+		width: calc(100vw / 2);
+		border-radius: 0.3rem;
+		color: #fff;
+		padding: 1rem;
+		font-size: 1rem;
+		border: 2px solid #faf4d3;
+		outline: #faf4d3;
+		resize: none;
+	}
+
+	pre {
+		background: #85756e;
+		width: calc(100vw / 2);
+		border-radius: 0.3rem;
+		color: #fff;
+		padding: 1rem;
+		white-space: pre-wrap;
+		overflow-y: scroll;
+	}
+</style>
+
+<script>
+	import { Remarkable } from 'remarkable';
+
+	const iframe = document.createElement('iframe');
+
+	var md = new Remarkable('full', {
+		html: true,
+		typographer: true,
+	});
+	let value = '';
+
+	$: output = `${md.render(value)}`;
+</script>
+
+<main>
+	<!-- <Editor /> -->
+	<textarea
+		name="editor"
+		id="editor"
+		cols="100"
+		rows="10"
+		spellcheck="true"
+		bind:value></textarea>
+
+	<!-- <Output value=/> -->
+	<pre>{@html output}</pre>
+</main>
